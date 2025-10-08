6d86f8a9ea059245b46f5a64984f014ac58d3625
S.Hariharasudhan <hariace027@gmail.com>
2025-10-08 11:03:40 +0530

    Create bin tree.py

---
+++ bin tree.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert_recursive(self.root, key)
+
+    def _insert_recursive(self, node, key):
+        if node is None:
+            return Node(key)
+        if key < node.key:
+            node.left = self._insert_recursive(node.left, key)
+        elif key > node.key:
+            node.right = self._insert_recursive(node.right, key)
+        return node
+
+    def inorder(self):
+        self._inorder_recursive(self.root)
+        print()
+
+    def _inorder_recursive(self, node):
+        if node:
+            self._inorder_recursive(node.left)
+            print(node.key, end=' ')
+            self._inorder_recursive(node.right)
+
+bst = BST()
+
+values = [50, 30, 70, 20, 40, 60, 80]
+for val in values:
+    bst.insert(val)
+print("Inorder traversal of BST:")
+bst.inorder()
+
+
